ada5bf6c3fd7402f18a56a933b34ed3c5595a05b
unknown <bielgabrielfs61@gmail.com>
2023-08-06 13:17:24 -0300

    first upload/ the base

---
+++ footballApi/main.js
@@ -0,0 +1,19 @@
+const id = 1062;
+const today = new Date().toISOString().split('T')[0];
+
+var myHeaders = new Headers();
+myHeaders.append("x-apisports-key", "97be4ab6f32b05d63a0150bbb002c6b2");
+myHeaders.append("x-rapidapi-host", "v3.football.api-sports.io");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+fetch(`https://v3.football.api-sports.io/fixtures?league=${id}&season=${2023}&date=${today}`, requestOptions)
+  .then(response => response.json())
+  .then(result => {
+    console.log(result);
+  })
+  .catch(error => console.log('error', error));
