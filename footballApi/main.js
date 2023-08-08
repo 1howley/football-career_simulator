42e7e3bb1d8b3ab65b9c227fad2ecb62b052ca24
NotHiaki <celsohenrique367@gmail.com>
2023-08-08 08:55:07 -0300

    styles

--- footballApi/main.js
+++ footballApi/main.js
@@ -17,13 +17,3 @@ fetch(`https://v3.football.api-sports.io/fixtures?league=${id}&season=${2023}&da
     console.log(result);
   })
   .catch(error => console.log('error', error));
-
-
-function setTeamSeleted() {
-  let age = 18;
-  let teamSelected = document.getElementById("teamSelected")
-  for(let x = 0; x < 9; x++) {
-    age += 2;
-    teamSelected.innerHTML += `<div class="ageCircle">${age}</div><div class="team"></div>`;
-  }
-}
